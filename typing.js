3260de369fdcf5a5a81088cb141e9e5861829253
pankaj-ramchandani <ramchandanipankaj787@gmail.com>
2023-05-01 13:54:54 +0530

    basic logic done

--- typing.js
+++ typing.js
@@ -1,9 +1,17 @@
 const words =
-  "Modern humans arrived on the Indian subcontinent from Africa no later than years ago Their long occupation initially in varying forms of isolation as hunter gatherers has made the region highly diverse second only to Africa in human genetic diversity ".split(
+  "modern humans arrived on the indian subcontinent from africa no later than years ago their long occupation initially in varying forms of isolation as hunter gatherers has made the region highly diverse second only to africa in human genetic diversity".split(
     " "
   );
 
 const wordsCount = words.length;
+
+function addClass(el, name) {
+  el.className += " " + name;
+}
+
+function removeClass(el, name) {
+  el.className = el.className.replace(name, "");
+}
 
 function randomWord() {
   const randomIndex = Math.ceil(Math.random() * wordsCount) - 1;
@@ -11,11 +19,9 @@ function randomWord() {
 }
 
 function formatWord(word) {
-  return `<div class="word">
-  <span class="letter">
-  ${word.split("").join('</span><span class="letter">')}
-</span>
-</div>`;
+  return `<div class="word"><span class="letter">${word
+    .split("")
+    .join('</span><span class="letter">')}</span></div>`;
 }
 
 function newGame() {
@@ -23,6 +29,86 @@ function newGame() {
   for (let i = 0; i < 200; i++) {
     document.getElementById("words").innerHTML += formatWord(randomWord());
   }
+  addClass(document.querySelector(".word"), "current");
+  addClass(document.querySelector(".letter"), "current");
 }
+
+document.getElementById("game").addEventListener("keyup", (e) => {
+  const key = e.key;
+  const currentWord = document.querySelector(".word.current");
+  const currentLetter = document.querySelector(".letter.current");
+  const expected = currentLetter?.innerHTML || " ";
+  const isLetter = key.length === 1 && key != " ";
+  const isSpace = key === " ";
+  const isBackspace = key === "Backspace";
+  const isFirstLetter = currentLetter === currentWord.firstChild;
+  console.log({ key, expected });
+
+  if (isLetter) {
+    if (currentLetter) {
+      addClass(currentLetter, key === expected ? "correct" : "incorrect");
+      removeClass(currentLetter, "current");
+      if (currentLetter.nextSibling) {
+        addClass(currentLetter.nextSibling, "current");
+      }
+    } else {
+      const incorrectLetter = document.createElement("span");
+      incorrectLetter.innerHTML = key;
+      incorrectLetter.className = "letter incorrect extra";
+      currentWord.appendChild(incorrectLetter);
+    }
+  }
+  if (isSpace) {
+    if (expected != " ") {
+      const letterToInvalidate = [
+        ...document.querySelectorAll(".word.current .letter:not(.correct)"),
+      ];
+      letterToInvalidate.forEach((letter) => {
+        addClass(letter, "incorrect");
+      });
+    }
+    removeClass(currentWord, "current");
+    addClass(currentWord.nextSibling, "current");
+    if (currentLetter) {
+      removeClass(currentLetter, "current");
+    }
+    addClass(currentWord.nextSibling.firstChild, "current");
+  }
+
+  if (isBackspace) {
+    if (currentLetter && isFirstLetter) {
+      // make prev word current, last letter current
+      removeClass(currentWord, "current");
+      addClass(currentWord.previousSibling, "current");
+      removeClass(currentLetter, "current");
+      addClass(currentLetter.previousSibling.lastChild, "current");
+      removeClass(currentWord.previousSibling.lastChild, "correct");
+      removeClass(currentWord.previousSibling.lastChild, "incorrect");
+    }
+    if (currentLetter && !isFirstLetter) {
+      // move back one letter, invalidate letter
+      removeClass(currentLetter, "current");
+      addClass(currentLetter.previousSibling, "current");
+      removeClass(currentLetter.previousSibling, "correct");
+      removeClass(currentLetter.previousSibling, "incorrect");
+    }
+    if (!currentLetter) {
+      addClass(currentWord.lastChild, "current");
+      removeClass(currentWord.lastChild, "correct");
+      removeClass(currentWord.lastChild, "incorrect");
+    }
+  }
+
+  // move cursor
+  const nextLetter = document.querySelector(".letter.current");
+  const nextWord = document.querySelector(".word.current");
+  const cursor = document.getElementById("cursor");
+  cursor.style.top =
+    (nextLetter || nextWord).getBoundingClientRect().top + 2 + "px";
+  cursor.style.left =
+    (nextLetter || nextWord).getBoundingClientRect()[
+      nextLetter ? "left" : "right"
+    ] + "px";
+});
 
 newGame();
